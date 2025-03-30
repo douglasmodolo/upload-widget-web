86f82e64462fb2bd02aef029a8fd775f17eee2c1
Douglas <douglasmodolo@outlook.com.br>
2025-03-30 14:54:18 -0300

    Animações do Widget

--- src/components/upload-widget-minimized-button.tsx
+++ src/components/upload-widget-minimized-button.tsx
@@ -4,7 +4,7 @@ import { UploadWidgetTitle } from "./upload-widget-title";
 
 export function UploadWidgetMinimizedButton() {
     return (
-        <Collapsible.CollapsibleTrigger className="group w-full bg-white/2 py-3 px-5 flex items-center justify-between cursor-pointer">
+        <Collapsible.CollapsibleTrigger className="group w-full bg-white/2 py-3 px-5 flex items-center justify-between cursor-pointer gap-10">
             <UploadWidgetTitle />
             
             <Maximize2 strokeWidth={1.5} className="size-4 text-zinc-400 group-hover:text-zinc-100" />            
